54ab3d2afc243ccbe9c96e9ebcad7f9e64dbd094
Ebullioscopic <hrhn.mudaliar251@gmail.com>
2024-10-02 19:08:50 +0530

    First commit

---
+++ StockViewer/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  StockViewer
+//
+//  Created by admin63 on 02/10/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ StockViewer/StockViewerApp.swift
@@ -0,0 +1,17 @@
+//
+//  StockViewerApp.swift
+//  StockViewer
+//
+//  Created by admin63 on 02/10/24.
+//
+
+import SwiftUI
+
+@main
+struct StockViewerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
